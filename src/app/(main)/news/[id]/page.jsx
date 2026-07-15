05e0f5eec52597319e3ed06dff82c670cbd5d4e3
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-07-15 22:32:56 +0600

    feat: added dynamic metadata

--- src/app/(main)/news/[id]/page.jsx
+++ src/app/(main)/news/[id]/page.jsx
@@ -3,6 +3,20 @@ import Image from "next/image";
 import Link from "next/link";
 import { FaArrowLeft } from "react-icons/fa";
 
+
+export const generateMetadata = async ({ params }) => {
+  const { id } = await params;
+  
+  const news = await getNewsById(id);
+  
+  return {
+    title: news.title,
+    description: news.details,
+  };
+};
+
+
+
 const NewsDetailsPage = async ({ params }) => {
   const { id } = await params;
 
