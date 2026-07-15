84dedb6c29b81203aceccb3477bd4b8e4515e376
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-07-15 11:58:04 +0600

    feat: header added

--- src/components/shared/Header/Header.jsx
+++ src/components/shared/Header/Header.jsx
@@ -1,18 +1,27 @@
-import React from 'react';
-import Logo from "@/assets/logo.png"
-import Image from 'next/image';
+import Image from "next/image";
+import Logo from "@/assets/logo.png";
+import { format } from "date-fns";
+
 const Header = () => {
-    return (
-      <div>
-        <Image
-          src={Logo}
-          alt="A image where write dragon news"
-          width={300}
-          height={200}
-        ></Image>
-        <p>Journalism Without Fear or Favour</p>
-      </div>
-    );
+  return (
+    <header className="py-10 flex flex-col items-center text-center">
+      <Image
+        src={Logo}
+        alt="Dragon News Logo"
+        width={420}
+        priority
+        className="w-auto h-auto"
+      />
+
+      <p className="text-[#706F6F] mt-4 text-lg">
+        Journalism Without Fear or Favour
+      </p>
+
+      <p className="mt-2 text-xl font-semibold text-[#403F3F]">
+        {format(new Date(), "EEEE, MMMM dd, yyyy")}
+      </p>
+    </header>
+  );
 };
 
 export default Header;
